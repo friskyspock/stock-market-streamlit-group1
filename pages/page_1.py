ab0cae96d58e36009c8a8324727a5473a808a018
friskyspock <aniketms42@outlook.com>
2023-07-05 22:45:58 +0530

    updated by aniket

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,40 +1,36 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
-import plotly.figure_factory as ff
-import yfinance as yf
-import datetime as dt
-import warnings
 import plotly.graph_objects as go
 
 from prophet import Prophet
 
+@st.cache_resource
+def build_model():
+    raw_data = st.session_state['data']
+    df = pd.DataFrame()
+    df['ds'] = raw_data.index
+    df['y'] = raw_data['Close'].values
+    model = Prophet(weekly_seasonality=False)
+    model.fit(df)
+    return model
 
 def main():
     st.markdown("# Page 2 ")
     st.sidebar.markdown("# Page 2 ")
 
     st.write(st.session_state['tickerName'])
+    
+    st.text_input("No. of days to predict", key="days")
 
-    st.write(st.session_state['data'].head())
-    # data = pd.DataFrame()
-    # data['ds'] = raw_data.index
-    # data['y'] = raw_data['Close'].values
+    if st.button('train model'):
+        future = build_model().make_future_dataframe(periods=int(st.session_state.days))
+        forecast = model.predict(future)
 
-    # st.text_input("No. of days to predict", key="days")
-
-    # if st.button('train model'):
-    #     model = Prophet(weekly_seasonality=False)
-    #     model.fit(data)
-    #     future = model.make_future_dataframe(periods=int(st.session_state.days))
-    #     forecast = model.predict(future)
-
-    #     fig = go.Figure()
-    #     fig.add_trace(go.Scatter(x=forecast['ds'][:-int(st.session_state.days)],y=forecast['yhat'][:-int(st.session_state.days)]))
-    #     fig.add_trace(go.Scatter(x=forecast['ds'][-int(st.session_state.days):],y=forecast['yhat'][-int(st.session_state.days):]))
-    #     st.plotly_chart(fig)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=forecast['ds'][:-int(st.session_state.days)],y=forecast['yhat'][:-int(st.session_state.days)]))
+        fig.add_trace(go.Scatter(x=forecast['ds'][-int(st.session_state.days):],y=forecast['yhat'][-int(st.session_state.days):]))
+        st.plotly_chart(fig)
 
 
 if __name__ == '__main__':
